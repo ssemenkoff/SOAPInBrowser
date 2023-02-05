58936411957d96494a1c88b1dce59491e18c4b04
ssemenkoff <ssemenkoff@gmail.com>
2023-02-05 12:11:23 +0400

    added conditional import from node env, latest fixes

--- src/http.ts
+++ src/http.ts
@@ -1,12 +1,17 @@
-import { fetch } from 'cross-fetch'
-
 export async function request (url: string | URL, data: string | null, exheaders: any = {}): Promise<Response> {
+  let fetchFunc
+  try {
+    fetchFunc = fetch
+  } catch (e) {
+    fetchFunc = require('cross-fetch')
+  }
+
   const baseHeaders = {
     Accept: 'text/html,application/xhtml+xml,application/xml',
     'Content-Type': 'text/xml'
   }
 
-  const request = await fetch(url, {
+  const request = await fetchFunc(url, {
     method: data ? 'POST' : 'GET',
     headers: {
       ...baseHeaders,
